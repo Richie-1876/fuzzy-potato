23582a418861b9b43e427963b19adf1926b6a610
salcedoe <esalcedo94@yahoo.com>
2020-03-13 14:32:48 -0700

    Delete and Update route working

--- calorie-tracker-back-end/controllers/food.js
+++ calorie-tracker-back-end/controllers/food.js
@@ -1,36 +1,57 @@
-const express = require('express')
-const food = express.Router()
-const Food = require('../models/Food.js')
+const express = require("express");
+const food = express.Router();
+const Food = require("../models/Food.js");
 // curl -X POST -H "Content-Type: application/json" -d '{"name":"pork", "calories": 400}' http://localhost:3003/foods
+// curl -X DELETE -H "Content-Type: application/json" -d '{"name":"pork", "calories": 400}' http://localhost:3003/foods/
+// curl -X PUT -H "Content-Type: application/json" -d '{"name":"pork", "calories": 400}' http://localhost:3003/foods/
+// curl -X PUT -H "Content-Type: application/json" -d '{"name":"I updated this"}' http://localhost:3003/foods/5e6bf81af379d00b48dd5547
 /**********************INDEX ROUTE**************************************/
 
-food.get('/', (req,res) => {
+food.get("/", (req, res) => {
   Food.find({}, (error, foundFoods) => {
-    if(error) {
-      res.status(400).json({error:err.message})
+    if (error) {
+      res.status(400).json({ error: error.message });
     }
-    res.status(200).json(foundFoods)
-  })
-})
+    res.status(200).json(foundFoods);
+  });
+});
 
 /*********************Create Route******************/
 
-food.post('/', async(req,res) => {
+food.post("/", async (req, res) => {
   Food.create(req.body, (error, createdFood) => {
     if (error) {
-      res.status(400).json({error:error.message})
+      res.status(400).json({ error: error.message });
     }
-    res.status(200).json(createdFood)
-  })
+    res.status(200).json(createdFood);
+  });
+});
+
+/**********************DELETE ROUTE***************************************/
+food.delete("/:id", (req, res) => {
+  Food.findByIdAndRemove(req.params.id, (error, deletedFood) => {
+    if (error) {
+      res.status(400).json({ error: error.message });
+    }
+    res.status(200).json(deletedFood);
+  });
+});
+
+/*********************UPDATE ROUTE******************************************/
+food.put("/:id", (req, res) => {
+  Food.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {new : true},
+    (error, updatedFood) =>{
+      if(error){
+        res.status(400).json({ error: error.message })
+      }
+      res.status(200).json(updatedFood)
+    }
+  )
 })
 
 
 
-/**********************DELETE ROUTE***************************************/
-
-
-
-/*********************UPDATE ROUTE******************************************/
-
-
-module.exports = food
+module.exports = food;
