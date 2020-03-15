d71bbb9f12b4cbde5151e6a01f3022e99fc9b801
salcedoe <esalcedo94@yahoo.com>
2020-03-15 10:18:21 -0700

    Update form almost working

--- calorie-tracker-front-end/src/App.js
+++ calorie-tracker-front-end/src/App.js
@@ -1,84 +1,110 @@
-import React from 'react';
-import NewForm from './components/NewForm'
+import React from "react";
+import NewForm from "./components/NewForm";
+import UpdateForm from "./components/UpdateForm";
 
-let baseURL = process.env.REACT_APP_BASEURL
+let baseURL = process.env.REACT_APP_BASEURL;
 
-if (process.env.NODE_ENV === 'development') {
-  baseURL = 'http://localhost:3003'
+if (process.env.NODE_ENV === "development") {
+  baseURL = "http://localhost:3003";
 }
-
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      foods: []
-    }
-    this.getFoods = this.getFoods.bind(this)
-    this.handleAddFood = this.handleAddFood.bind(this)
-    this.deleteFood = this.deleteFood.bind(this)
+      foods: [],
+      isEditing: false
+    };
+    this.getFoods = this.getFoods.bind(this);
+    this.handleAddFood = this.handleAddFood.bind(this);
+    this.deleteFood = this.deleteFood.bind(this);
+    this.handleEditButton = this.handleEditButton.bind(this);
   }
-  componentDidMount(){
-    this.getFoods()
+  componentDidMount() {
+    this.getFoods();
   }
   async getFoods() {
     try {
-      let response = await fetch(`${baseURL}/foods`)
-      let data = await response.json()
+      let response = await fetch(`${baseURL}/foods`);
+      let data = await response.json();
       this.setState({
         foods: data
-      })
-
-    } catch(e) {
+      });
+    } catch (e) {
       console.error(e);
     }
   }
 
-  handleAddFood(food){
-    const copyFoods = [food, ...this.state.foods]
+  handleAddFood(food) {
+    const copyFoods = [food, ...this.state.foods];
     this.setState({
-      foods : copyFoods
-    })
+      foods: copyFoods
+    });
+  }
+
+  handleEditButton() {
+    this.setState({ isEditing: !this.state.isEditing });
   }
 
   async deleteFood(id) {
-    
     try {
       let response = await fetch(`${baseURL}/foods/${id}`, {
-        method: 'DELETE'
-      })
-      let data = await response.json()
-      const foundFood = this.state.foods.findIndex(food => food._id === id)
-      const copyFoods = [...this.state.foods]
-      copyFoods.splice(foundFood, 1)
-      this.setState({foods: copyFoods})
-
-
+        method: "DELETE"
+      });
+      let data = await response.json();
+      const foundFood = this.state.foods.findIndex(food => food._id === id);
+      const copyFoods = [...this.state.foods];
+      copyFoods.splice(foundFood, 1);
+      this.setState({ foods: copyFoods });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-
   }
 
   render() {
-    return(
+    return (
       <div>
         <h1>Calorie Counter</h1>
-        <NewForm handleAddFood={this.handleAddFood}/>
+        <NewForm handleAddFood={this.handleAddFood} />
         <ul>
-        {
-          this.state.foods.map(food => {
-            return(
-              <div key={food._id}>
-              <li >{food.name}:{food.calories}{' '} calories</li>
-              <button onClick={()=>{this.deleteFood(food._id)}}>DELETE</button>
+          {this.state.foods.map(food => {
+            return (
+              <div>
+                <div key={food._id}>
+                  <li>
+                    {food.name}:{food.calories} calories
+                  </li>
+                  <button
+                    onClick={() => {
+                      this.deleteFood(food._id);
+                    }}
+                  >
+                    DELETE
+                  </button>
+                  <button
+                    onClick={() => {
+                      this.handleEditButton();
+                    }}
+                  >
+                    EDIT
+                  </button>
+                  {this.state.isEditing ? (
+                    <UpdateForm
+                      foods={this.state.foods}
+                      handleChange={this.handleChange}
+                      food={food}
+                      handleEditFood={this.handleEditFood}
+                    />
+                  ) : (
+                    " "
+                  )}
+                </div>
               </div>
-            )
-          })
-        }
+            );
+          })}
         </ul>
       </div>
-    )
+    );
   }
 }
 export default App;
